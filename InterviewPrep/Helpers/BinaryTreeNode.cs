11c5f928a51268803d1fdec0d8bbd28b8debfa2d
Kyle Wall <Darker1@gmail.com>
2016-03-15 22:30:02 -0700

    Built Binary tree node, and Question to go with it

---
+++ InterviewPrep/Helpers/BinaryTreeNode.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace InterviewPrep.Helpers
+{
+    public class BinaryTreeNode<T> : IComparable, IComparable<BinaryTreeNode<T>> where T : IComparable
+    {
+        public T Data { get; set; }
+        public BinaryTreeNode<T> Left { get; set; }
+        public BinaryTreeNode<T> Right { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            return CompareTo(obj as BinaryTreeNode<T>); 
+        }
+
+        public int CompareTo(BinaryTreeNode<T> other)
+        {
+            var data = Data.CompareTo(other.Data);
+            if(data != 0)
+                return data;
+
+            int left = NodeCheck(Left, other.Left);
+            if (left != 0)
+                return left;
+
+            int right = NodeCheck(Right, other.Right);
+            if (right != 0)
+                return right;
+            
+            return 0;
+        }
+
+        private static int NodeCheck(BinaryTreeNode<T> node1, BinaryTreeNode<T> node2)
+        {
+            if (BothNull(node1, node2))
+                return 0;
+
+            if (CanCheckNodes(node1, node2))
+                return node1.CompareTo(node2);
+
+            return node1 == null ? -1 : 1;
+        }
+
+        private static bool CanCheckNodes(BinaryTreeNode<T> node1, BinaryTreeNode<T> node2)
+        {
+            return (node1 != null && node2 != null);
+        }
+
+        private static bool BothNull(BinaryTreeNode<T> node1, BinaryTreeNode<T> node2)
+        {
+            return node1 == null && node2 == null;
+        }
+    }
+}
---
+++ InterviewPrep/Questions/BuildBinaryTree.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InterviewPrep.Helpers;
+
+namespace InterviewPrep.Questions
+{
+    public class BuildBinaryTree : Runable, IQuestion<int, BinaryTreeNode<bool>>
+    {
+        public ExpectedInputAndOutput<int, BinaryTreeNode<bool>>[] TestValues {
+            get
+            {
+                return new[]
+                {
+                    new ExpectedInputAndOutput<int, BinaryTreeNode<bool>>() {Input = 0, Output = new BinaryTreeNode<bool>()},
+                    new ExpectedInputAndOutput<int, BinaryTreeNode<bool>>() {Input = 1, Output = new BinaryTreeNode<bool>() {Left = new BinaryTreeNode<bool>(), Right = new BinaryTreeNode<bool>()} },
+                    new ExpectedInputAndOutput<int, BinaryTreeNode<bool>>() {Input = 2, Output = new BinaryTreeNode<bool>() {
+                        Left = new BinaryTreeNode<bool>() { Left = new BinaryTreeNode<bool>(), Right = new BinaryTreeNode<bool>()},
+                        Right = new BinaryTreeNode<bool>() { Left = new BinaryTreeNode<bool>(), Right = new BinaryTreeNode<bool>()}} },
+                };
+            }
+        }
+
+        public BinaryTreeNode<bool> Run(int input)
+        {
+            var node = new BinaryTreeNode<bool>();
+            if (input == -1)
+                return null;
+
+            node.Left = Run(input - 1);
+            node.Right = Run(input - 1);
+
+            return node;
+        }
+
+        public string QuestionName {
+            get { return "Build a binary tree of height N.  Each node shall contain a bool with False as Data."; }
+        }
+    }
+}
